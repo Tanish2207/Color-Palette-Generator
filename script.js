a840d061d8d019e7ef973b4f23646a3db792c582
Tanish2207 <bhamare.tn@gmail.com>
2024-03-04 23:02:09 +0530

    Changed button of CTC

--- script.js
+++ script.js
@@ -100,7 +100,6 @@ function copyToClipboard() {
   var copyText = "FFF";
   navigator.clipboard.writeText(copyText).then(() => {
     // Alert the user that the action took place.
-    // Nobody likes hidden stuff being done under the hood!
     alert("Copied to clipboard");
   });
 }
